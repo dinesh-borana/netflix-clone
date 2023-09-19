23ca79330a504ed2a1862f0e66b9d06c0b195617
Dinesh Kumar <dinesh.kum@media.net>
2023-09-19 11:53:37 +0530

    moved to typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import Body from './utils/Body';
+
+import './App.css';
+
+function App() {
+  return (
+   <Body />
+  );
+}
+
+export default App;
